fa254e2ec7e261643384b11d6c9fde832b2fa027
livingspring <309334941@qq.com>
2024-04-18 20:48:29 -0500

    Initial Commit

---
+++ ValiApp/ValiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ValiAppApp.swift
+//  ValiApp
+//
+//  Created by RongWei Ji on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct ValiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
